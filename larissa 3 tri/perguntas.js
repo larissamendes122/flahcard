e56e73cddc6f20077023480edbd502f909b26cd4
larissamendes122 <161464972+larissamendes122@users.noreply.github.com>
2024-11-04 11:07:43 -0300

    Add files via upload

---
+++ larissa 3 tri/perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+
+criaCartao(
+    'programação',
+    'O que é uma função?',
+    'Uma função é um bloco de codigo que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês é HI (RAI)'
+)
